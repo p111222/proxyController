28431c03b67cf06fb80d03dd36d603af91406bc7
p111222 <princejainpre.ram@gmail.com>
2024-10-10 16:59:56 +0530

    opentelemetry added

--- rest-template-gateway/nishkaiv/src/main/java/com/nishkaiv/nishkaiv/Controller/ProxyController.java
+++ rest-template-gateway/nishkaiv/src/main/java/com/nishkaiv/nishkaiv/Controller/ProxyController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -142,6 +143,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping("/api")
 public class ProxyController {
 
     private final RestTemplate restTemplate = new RestTemplate();
